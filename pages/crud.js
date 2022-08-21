07092bae86d14056d9d8ae62510c37df88cae90a
LAPTOP-V096CHQI\ACER <latifnazarudin17@gmail.com>
2022-08-21 22:45:20 +0700

    explor fetching serverside

--- pages/crud.js
+++ pages/crud.js
@@ -1,5 +1,18 @@
 import Head from "next/head";
-const Crud = () => {
+
+export async function getServerSideProps(context) {
+  console.log(context);
+  // fetching data
+  const fetching = await fetch("http://jsonplaceholder.typicode.com/posts");
+  const data = await fetching.json();
+  return {
+    props: { data },
+  };
+}
+
+const Crud = (props) => {
+  // console.logs(props);
+  const { data } = props;
   return (
     <>
       <Head>
@@ -8,6 +21,10 @@ const Crud = () => {
       </Head>
       <div>
         <h2>Halaman CRUD</h2>
+        {/* looping */}
+        {data.map((item) => {
+          return <p>{item.body}</p>;
+        })}
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 export default function Home() {
   return (
     <div>
@@ -7,7 +8,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" content="Halaman about" />
       </Head>
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl">Selamat datang di Explore NextJs</h1>
+        {/* <h1 class="text-3xl">Selamat datang di Explore NextJs</h1> */}
+        <Image src="/next.png" width="250" height="250" />
       </div>
     </div>
   );
